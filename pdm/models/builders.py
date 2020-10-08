08e6f86b261f30271920846c24e5f6f9b8b56154
Frost Ming <mianghong@gmail.com>
2020-10-08 09:27:23 +0800

    build egg info in isolated env

--- pdm/models/builders.py
+++ pdm/models/builders.py
@@ -4,12 +4,10 @@ import sys
 
 from build import ProjectBuilder
 from build.env import IsolatedEnvironment
-from pep517.wrappers import default_subprocess_runner
 
 from pdm.exceptions import BuildError
 from pdm.iostream import stream
 from pdm.pep517.base import Builder
-from pdm.utils import cd
 
 _SETUPTOOLS_SHIM = (
     "import sys, setuptools, tokenize; sys.argv[0] = {0!r}; __file__={0!r};"
@@ -28,23 +26,18 @@ def log_subprocessor(cmd, cwd=None, extra_environ=None):
     proc = subprocess.run(cmd, cwd=cwd, env=env, capture_output=capture_output)
     if capture_output:
         stream.logger.debug(proc.stdout)
-        stream.logger.debug(proc.stderr)
     if proc.returncode:
-        raise BuildError(f"Run command {cmd} return non-zero status.")
+        stream.logger.debug(proc.stderr)
+        raise BuildError(f"Call command {cmd} return non-zero status.")
 
 
 def build_wheel(src_dir: str, out_dir: str) -> str:
     """Build wheel and return the full path of the artifact."""
     builder = ProjectBuilder(srcdir=src_dir)
     stream.echo("Building wheel...")
-    runner = (
+    with IsolatedEnvironment.for_current() as env, builder.hook.subprocess_runner(
         log_subprocessor
-        if stream.verbosity < stream.DETAIL
-        else default_subprocess_runner
-    )
-    with IsolatedEnvironment.for_current() as env, cd(
-        src_dir
-    ), builder.hook.subprocess_runner(runner):
+    ):
         env.install(builder.build_dependencies)
         filename = builder.hook.build_wheel(out_dir)
     stream.echo(f"Built {filename}")
@@ -55,14 +48,9 @@ def build_sdist(src_dir: str, out_dir: str) -> str:
     """Build sdist and return the full path of the artifact."""
     builder = ProjectBuilder(srcdir=src_dir)
     stream.echo("Building sdist...")
-    runner = (
+    with IsolatedEnvironment.for_current() as env, builder.hook.subprocess_runner(
         log_subprocessor
-        if stream.verbosity < stream.DETAIL
-        else default_subprocess_runner
-    )
-    with IsolatedEnvironment.for_current() as env, cd(
-        src_dir
-    ), builder.hook.subprocess_runner(runner):
+    ):
         env.install(builder.build_dependencies)
         filename = builder.hook.build_sdist(out_dir)
     stream.echo(f"Built {filename}")
@@ -83,16 +71,12 @@ def build_egg_info(src_dir: str, out_dir: str) -> str:
     # Ignore destination since editable builds should be build locally
     builder = Builder(src_dir)
     setup_py_path = builder.ensure_setup_py().as_posix()
-    with cd(src_dir):
+    with IsolatedEnvironment.for_current() as env:
+        env.install(["setuptools"])
         args = [sys.executable, "-c", _SETUPTOOLS_SHIM.format(setup_py_path)]
         args.extend(["egg_info", "--egg-base", out_dir])
         stream.echo("Building egg info...")
-        runner = (
-            log_subprocessor
-            if stream.verbosity < stream.DETAIL
-            else default_subprocess_runner
-        )
-        runner(args)
+        log_subprocessor(args, cwd=src_dir)
         filename = _find_egg_info(out_dir)
     stream.echo(f"Built {filename}")
     return os.path.join(out_dir, filename)
