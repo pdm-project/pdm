9b40b3719f9d4991252f2e130e9cb9d4322b58d3
Frost Ming <mianghong@gmail.com>
2020-12-14 10:37:40 +0800

    release script

---
+++ tasks/release.py
@@ -0,0 +1,81 @@
+import argparse
+import subprocess
+import sys
+from pathlib import Path
+
+import click
+import parver
+
+PROJECT_DIR = Path(__file__).parent.parent
+
+
+def get_current_version():
+    from pdm.core import Project
+
+    project = Project(PROJECT_DIR)
+    return project.meta.version
+
+
+def bump_version(pre=None, major=False, minor=False, patch=True):
+    if not any([pre, major, minor, patch]):
+        patch = True
+    if len([v for v in [pre, major, minor, patch] if v]) != 1:
+        click.secho(
+            "Only one option should be provided among "
+            "(--pre, --major, --minor, --patch)",
+            fg="red",
+            err=True,
+        )
+        sys.exit(1)
+    current_version = parver.Version.parse(get_current_version())
+    if pre:
+        version = current_version.bump_pre(pre)
+    elif major or minor:
+        version_idx = [major, minor, patch].index(True)
+        version = current_version.bump_release(index=version_idx)
+    else:
+        version = current_version
+    version = version.replace(local=None, dev=None)
+    return str(version)
+
+
+def release(dry_run=False, commit=True, pre=None, major=False, minor=False, patch=True):
+    new_version = bump_version(pre, major, minor, patch)
+    click.secho(f"Bump version to: {new_version}", fg="yellow")
+    if dry_run:
+        subprocess.check_call(["towncrier", "--version", new_version, "--draft"])
+    else:
+        subprocess.check_call(["towncrier", "--yes", "--version", new_version])
+        subprocess.check_call(["git", "add", "."])
+        if commit:
+            subprocess.check_call(["git", "commit", "-m", f"Release {new_version}"])
+            subprocess.check_call(
+                ["git", "tag", "-a", new_version, "-m", f"v{new_version}"]
+            )
+            subprocess.check_call(["git", "push"])
+            subprocess.check_call(["git", "push", "--tags"])
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser("release.py")
+
+    parser.add_argument("--dry-run", action="store_true", help="Dry run mode")
+    parser.add_argument(
+        "--no-commit",
+        action="store_false",
+        dest="commit",
+        default=True,
+        help="Do not commit to Git",
+    )
+    group = parser.add_argument_group(title="version part")
+    group.add_argument("--pre", help="Pre tag")
+    group.add_argument("--major", action="store_true", help="Bump major version")
+    group.add_argument("--minor", action="store_true", help="Bump minor version")
+    group.add_argument("--patch", action="store_true", help="Bump patch version")
+
+    return parser.parse_args(argv)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    release(args.dry_run, args.commit, args.pre, args.major, args.minor, args.patch)
