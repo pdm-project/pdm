7db6c9ccf4f0efb17aff1664414085950e3ca439
Frost Ming <mianghong@gmail.com>
2020-12-16 12:56:49 +0800

    fix pkg_resources import

--- pdm/__init__.py
+++ pdm/__init__.py
@@ -1,3 +1,14 @@
 from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
+
+
+def _fix_pkg_resources():
+    import importlib
+    import sys
+
+    sys.modules["pkg_resources"] = importlib.import_module("pip._vendor.pkg_resources")
+
+
+_fix_pkg_resources()
+del _fix_pkg_resources
