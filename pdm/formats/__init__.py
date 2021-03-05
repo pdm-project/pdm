6343fbf979e5ea5506f5442f3cafd10d07cfca34
Frost Ming <mianghong@gmail.com>
2021-03-05 15:18:08 +0800

    Remove protocol that is introduced in Py38

--- pdm/formats/__init__.py
+++ pdm/formats/__init__.py
@@ -1,34 +1,8 @@
 from __future__ import annotations
 
-import os
-from typing import TYPE_CHECKING, Dict, Iterable, Protocol, Tuple
-
 from pdm.formats import flit, legacy, pipfile, poetry, requirements, setup_py
 
-if TYPE_CHECKING:
-    import argparse
-
-    from pdm.models.candidates import Candidate
-    from pdm.project import Project
-
-
-class FormatConverter(Protocol):
-    def check_fingerprint(self, project: Project, filename: os.PathLike) -> bool:
-        ...
-
-    def convert(self, project: Project, filename: os.PathLike) -> Tuple[dict, dict]:
-        ...
-
-    def export(
-        self,
-        project: Project,
-        candidates: Iterable[Candidate],
-        options: argparse.Namespace,
-    ) -> str:
-        ...
-
-
-FORMATS: Dict[str, FormatConverter] = {
+FORMATS = {
     "pipfile": pipfile,
     "poetry": poetry,
     "flit": flit,
