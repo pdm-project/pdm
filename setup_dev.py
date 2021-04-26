e37e8f2810c48efea8dcdecd2d1f7c158abd03b0
林玮 (Jade Lin) <linw1995@icloud.com>
2021-04-26 18:51:47 +0800

    Fix development setup error due to the unreleased fix
    resolve #415

--- setup_dev.py
+++ setup_dev.py
@@ -26,9 +26,8 @@ def main():
     subprocess.check_call([sys.executable, "-m", "venv", venv_path.as_posix()])
 
     print("Installing base requirements...", flush=True)
-    subprocess.check_call(
-        [venv_python.as_posix(), "-m", "pip", "install", "-U", "pip", "pdm"]
-    )
+    subprocess.check_call([venv_python.as_posix(), "-m", "pip", "install", "-U", "pip"])
+    subprocess.check_call([venv_python.as_posix(), "-m", "pip", "install", "."])
 
     print("Setup project for development...", flush=True)
     venv_pdm = (venv_python.parent / "pdm").as_posix()
