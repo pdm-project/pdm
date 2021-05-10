21dbba3fa07d135d9c1e648e930cc46722df7b6f
Frost Ming <mianghong@gmail.com>
2021-05-10 09:41:49 +0800

    get hash from remote file candiddate Close #450

--- pdm/models/repositories.py
+++ pdm/models/repositories.py
@@ -191,7 +191,10 @@ class BaseRepository:
             return candidate.hashes
         req = candidate.req.copy()
         req.specifier = SpecifierSet(f"=={candidate.version}")
-        matching_candidates = self.find_candidates(req, allow_all=True)
+        if candidate.req.is_file_or_url:
+            matching_candidates = [candidate]
+        else:
+            matching_candidates = self.find_candidates(req, allow_all=True)
         with self.environment.get_finder(self.sources) as finder:
             self._hash_cache.session = finder.session
             return {
