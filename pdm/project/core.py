1ee8526090d61c4c80c0a7ca4dfd4e59ced60dd8
Angus Hollands <goosey15@gmail.com>
2021-11-22 14:57:47 +0000

    Refactor: remove environment from project's public API
    Given that the project creates the environment, let's keep ownership
    of it exclusively with project.

--- pdm/project/core.py
+++ pdm/project/core.py
@@ -214,10 +214,6 @@ class Project:
             self._environment = self.get_environment()
         return self._environment
 
-    @environment.setter
-    def environment(self, value: Environment) -> None:
-        self._environment = value
-
     @property
     def python_requires(self) -> PySpecSet:
         return PySpecSet(self.meta.requires_python)
