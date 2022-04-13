3e3064b595adcc25cb87bd953d30077ccc75d6a4
林玮 (Jade Lin) <linw1995@icloud.com>
2022-04-14 01:14:03 +0800

    Fix use_cache.json with corrupted python causes `pdm use` error

--- pdm/cli/actions.py
+++ pdm/cli/actions.py
@@ -29,7 +29,12 @@ from pdm.cli.utils import (
     set_env_in_reg,
     translate_groups,
 )
-from pdm.exceptions import NoPythonVersion, PdmUsageError, ProjectError
+from pdm.exceptions import (
+    InvalidPyVersion,
+    NoPythonVersion,
+    PdmUsageError,
+    ProjectError,
+)
 from pdm.formats import FORMATS
 from pdm.formats.base import array_of_inline_tables, make_array, make_inline_table
 from pdm.models.caches import JSONFileCache
@@ -568,14 +573,23 @@ def do_use(
     selected_python: PythonInfo | None = None
     if python and not ignore_remembered:
         if use_cache.has_key(python):
-            cached_python = PythonInfo.from_path(use_cache.get(python))
-            if version_matcher(cached_python):
+            path = use_cache.get(python)
+            try:
+                cached_python = PythonInfo.from_path(path)
+                if version_matcher(cached_python):
+                    project.core.ui.echo(
+                        "Using the last selection, add '-i' to ignore it.",
+                        fg="yellow",
+                        err=True,
+                    )
+                    selected_python = cached_python
+            except InvalidPyVersion:
                 project.core.ui.echo(
-                    "Using the last selection, add '-i' to ignore it.",
-                    fg="yellow",
+                    f"The last selection is corrupted. {path!r}",
+                    fg="red",
                     err=True,
                 )
-                selected_python = cached_python
+
     if selected_python is None:
         found_interpreters = list(dict.fromkeys(project.find_interpreters(python)))
         matching_interperters = list(filter(version_matcher, found_interpreters))
