7887c4ba68cb1b210c67dfbb5eac05da0eba35ed
Frost Ming <me@frostming.com>
2023-01-09 15:33:31 +0800

    docs: add pdm expansions in header

---
+++ docs/docs/assets/extra.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const expansionRepo = 'https://github.com/pdm-project/pdm-expansions';
+  const expansionsApi = 'https://pdm-expansions.vercel.app/api/sample';
+  const element = document.querySelector('a.pdm-expansions');
+
+  function loadExpansions() {
+    fetch(expansionsApi, { mode: 'cors', redirect: 'follow' })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        window.expansionList = data.data;
+        setExpansion();
+      });
+  }
+
+  function setExpansion() {
+    const { expansionList } = window;
+    if (!expansionList || !expansionList.length) {
+      window.location.href = expansionRepo;
+      return;
+    }
+    const expansion = expansionList[expansionList.length - 1];
+    expansionList.splice(expansionList.length - 1, 1);
+    element.innerText = expansion;
+  }
+  loadExpansions();
+  element.addEventListener('click', function (e) {
+    e.preventDefault();
+    setExpansion();
+  });
+});
