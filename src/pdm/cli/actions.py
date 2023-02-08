6e751d4fa541392687b1a984edae893e6dd34291
Frost Ming <mianghong@gmail.com>
2023-02-08 19:10:52 +0800

    fix: Fix the log leakage in installation (#1689)

--- src/pdm/cli/actions.py
+++ src/pdm/cli/actions.py
@@ -194,7 +194,7 @@ def do_sync(
     candidates = resolve_candidates_from_lockfile(project, requirements)
     if tracked_names and dry_run:
         candidates = {name: c for name, c in candidates.items() if name in tracked_names}
-    handler = project.core.synchronizer_class(
+    synchronizer = project.core.synchronizer_class(
         candidates,
         project.environment,
         clean,
@@ -205,9 +205,10 @@ def do_sync(
         reinstall=reinstall,
         only_keep=only_keep,
     )
-    hooks.try_emit("pre_install", candidates=candidates, dry_run=dry_run)
-    handler.synchronize()
-    hooks.try_emit("post_install", candidates=candidates, dry_run=dry_run)
+    with project.core.ui.logging("install"):
+        hooks.try_emit("pre_install", candidates=candidates, dry_run=dry_run)
+        synchronizer.synchronize()
+        hooks.try_emit("post_install", candidates=candidates, dry_run=dry_run)
 
 
 def do_add(
