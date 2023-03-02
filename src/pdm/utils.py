6d8f19d0acc07ac9167d58f41453936ec1c5a21e
Frost Ming <me@frostming.com>
2023-03-02 10:04:15 +0800

    fix: Replace `.` with `-` when normalizing package name Close #1745

--- src/pdm/utils.py
+++ src/pdm/utils.py
@@ -325,7 +325,7 @@ def get_rev_from_url(url: str) -> str:
 
 
 def normalize_name(name: str, lowercase: bool = True) -> str:
-    name = re.sub(r"[^A-Za-z0-9.]+", "-", name)
+    name = re.sub(r"[^A-Za-z0-9]+", "-", name)
     return name.lower() if lowercase else name
 
 
