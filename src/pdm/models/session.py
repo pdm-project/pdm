d7143118717099ba8058658da8669e1e4daaa373
Frost Ming <me@frostming.com>
2023-05-05 19:01:12 +0800

    fix: patch cachecontrol instead of pinning urllib3
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pdm/models/session.py
+++ src/pdm/models/session.py
@@ -3,10 +3,24 @@ from pathlib import Path
 from typing import Any
 
 from cachecontrol.adapter import CacheControlAdapter
+from cachecontrol.serialize import Serializer as LegacySerializer
 from requests_toolbelt.utils import user_agent
 from unearth.session import InsecureMixin, PyPISession
 
 from pdm.__version__ import __version__
+
+
+class Serializer(LegacySerializer):
+    def dumps(self, request, response, body=None):  # type: ignore[no-untyped-def]
+        if not hasattr(response, "strict"):
+            # XXX: urllib3 2.0 removes this attribute
+            response.strict = False
+        return super().dumps(request, response, body)
+
+    def prepare_response(self, request, cached, body_file=None):  # type: ignore[no-untyped-def]
+        # We don't need to pass strict to HTTPResponse
+        cached["response"].pop("strict", None)
+        return super().prepare_response(request, cached, body_file)
 
 
 class InsecureCacheControlAdapter(InsecureMixin, CacheControlAdapter):
@@ -18,8 +32,9 @@ class PDMSession(PyPISession):
         from pdm.models.caches import SafeFileCache
 
         cache = SafeFileCache(str(cache_dir))
-        self.secure_adapter_cls = functools.partial(CacheControlAdapter, cache=cache)
-        self.insecure_adapter_cls = functools.partial(InsecureCacheControlAdapter, cache=cache)
+        serializer = Serializer()
+        self.secure_adapter_cls = functools.partial(CacheControlAdapter, cache=cache, serializer=serializer)
+        self.insecure_adapter_cls = functools.partial(InsecureCacheControlAdapter, cache=cache, serializer=serializer)
         super().__init__(**kwargs)
         self.headers["User-Agent"] = self._make_user_agent()
 
