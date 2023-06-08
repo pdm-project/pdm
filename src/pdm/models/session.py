b40c58e5c248e067df41f2db46ac6748480aca01
Frost Ming <me@frostming.com>
2023-06-08 11:00:41 +0800

    fix: no crash when reading the old cache Fixes #1981
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pdm/models/session.py
+++ src/pdm/models/session.py
@@ -1,10 +1,15 @@
+from __future__ import annotations
+
 import functools
 from pathlib import Path
-from typing import Any
+from typing import IO, Any, Mapping
 
 from cachecontrol.adapter import CacheControlAdapter
+from cachecontrol.serialize import Serializer
+from requests import Request
 from requests_toolbelt.utils import user_agent
 from unearth.session import InsecureMixin, PyPISession
+from urllib3 import HTTPResponse
 
 from pdm.__version__ import __version__
 
@@ -13,13 +18,32 @@ class InsecureCacheControlAdapter(InsecureMixin, CacheControlAdapter):
     pass
 
 
+class CompatibleSerializer(Serializer):
+    """We've switched the cache to SeparateBodyCache since 2.7.1, we use this serializer to
+    read the old cache. However, reading the new cache with older PDM versions will still
+    result in a broken cache.
+    """
+
+    def prepare_response(
+        self, request: Request, cached: Mapping[str, Any], body_file: IO[bytes] | None = None
+    ) -> HTTPResponse | None:
+        body_raw = cached["response"].get("body")
+        if not body_raw and body_file is None:
+            # When we update the old cache using SeparateBodyCache, body_raw is set to empty
+            # but the body_file hasn't been created yet. The cache is broken at this point.
+            # return None to ignore this entry.
+            return None
+        return super().prepare_response(request, cached, body_file)
+
+
 class PDMSession(PyPISession):
     def __init__(self, *, cache_dir: Path, **kwargs: Any) -> None:
         from pdm.models.caches import SafeFileCache
 
         cache = SafeFileCache(str(cache_dir))
-        self.secure_adapter_cls = functools.partial(CacheControlAdapter, cache=cache)
-        self.insecure_adapter_cls = functools.partial(InsecureCacheControlAdapter, cache=cache)
+        serializer = CompatibleSerializer()
+        self.secure_adapter_cls = functools.partial(CacheControlAdapter, cache=cache, serializer=serializer)
+        self.insecure_adapter_cls = functools.partial(InsecureCacheControlAdapter, cache=cache, serializer=serializer)
         super().__init__(**kwargs)
         self.headers["User-Agent"] = self._make_user_agent()
 
