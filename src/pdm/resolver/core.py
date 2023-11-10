5887ad20afea82866d6883d2cc497dd739805c9f
Frost Ming <mianghong@gmail.com>
2023-11-10 17:11:09 +0800

    feat: add a new setting `package-type` to distinguish application and library (#2394)

--- src/pdm/resolver/core.py
+++ src/pdm/resolver/core.py
@@ -45,7 +45,9 @@ def resolve(
     mapping = cast(Dict[str, Candidate], result.mapping)
     mapping.pop("python", None)
 
-    local_name = normalize_name(repository.environment.project.name) if repository.environment.project.name else None
+    local_name = (
+        normalize_name(repository.environment.project.name) if repository.environment.project.is_library else None
+    )
     for key, candidate in list(result.mapping.items()):
         if key is None:
             continue
