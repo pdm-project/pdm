974678007f9939dae8a6368a31c9064de16ca0aa
Frost Ming <me@frostming.com>
2024-03-21 13:33:46 +0800

    removal: remove deprecated pdm.models.environment (#2710)
    * removal: remove deprecated pdm.models.environment
    
    Signed-off-by: Frost Ming <me@frostming.com>
    
    * add news
    
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pdm/environments/__init__.py
+++ src/pdm/environments/__init__.py
@@ -1,20 +1,6 @@
-from typing import Any
-
 from pdm.environments.base import BareEnvironment, BaseEnvironment
 from pdm.environments.local import PythonLocalEnvironment
 from pdm.environments.python import PythonEnvironment
-from pdm.utils import deprecation_warning
-
-_deprecated = {"Environment": PythonLocalEnvironment, "GlobalEnvironment": PythonEnvironment}
-
-
-def __getattr__(name: str) -> Any:
-    if name in _deprecated:
-        real = _deprecated[name]
-        deprecation_warning(f"{name} is deprecated, please use {real.__name__} instead", stacklevel=2)
-        return real
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
-
 
 __all__ = [
     "BaseEnvironment",
@@ -22,4 +8,3 @@ __all__ = [
     "PythonEnvironment",
     "PythonLocalEnvironment",
 ]
-__all__.extend(_deprecated)
