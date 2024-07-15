213158fa2216231cac57d999484e755a09d923ab
Frost Ming <me@frostming.com>
2024-07-15 18:02:59 +0800

    fix: log sdist
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pdm/builders/sdist.py
+++ src/pdm/builders/sdist.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 
 from pdm.builders.base import EnvBuilder, wrap_error
+from pdm.termui import logger
 
 
 class SdistBuilder(EnvBuilder):
@@ -14,5 +15,8 @@ class SdistBuilder(EnvBuilder):
             self.install(self._requires, shared=True)
             requires = self._hook.get_requires_for_build_sdist(self.config_settings)
             self.install(requires)
+        lib = self._env.get_paths()["purelib"]
+        logger.info("Libs: %s", os.listdir(lib))
+        logger.info("PYTHONPATH: %s", self._env_vars["PYTHONPATH"])
         filename = self._hook.build_sdist(out_dir, self.config_settings)
         return os.path.join(out_dir, filename)
