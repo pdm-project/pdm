0e5f02893f203621ede3b1b1386d5cebd3cfe0ba
Frost Ming <me@frostming.com>
2024-07-15 19:14:57 +0800

    fix: remove debug logs
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pdm/builders/sdist.py
+++ src/pdm/builders/sdist.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 
 from pdm.builders.base import EnvBuilder, wrap_error
-from pdm.termui import logger
 
 
 class SdistBuilder(EnvBuilder):
@@ -15,8 +14,5 @@ class SdistBuilder(EnvBuilder):
             self.install(self._requires, shared=True)
             requires = self._hook.get_requires_for_build_sdist(self.config_settings)
             self.install(requires)
-        lib = self._env.get_paths()["purelib"]
-        logger.info("Libs(%s): %s", lib, os.listdir(lib))
-        logger.info("PYTHONPATH: %s", self._env_vars["PYTHONPATH"])
         filename = self._hook.build_sdist(out_dir, self.config_settings)
         return os.path.join(out_dir, filename)
