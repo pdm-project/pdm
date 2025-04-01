f34fc38eb98e8f973e3df3f177390ac41eee5b06
Frost Ming <me@frostming.com>
2025-04-01 18:07:13 +0800

    fix: add tests
    Signed-off-by: Frost Ming <me@frostming.com>

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -118,3 +118,31 @@ def test_project_plugin_library(pdm, project, core, monkeypatch):
         core.load_plugins()
         result = pdm(["hello", "Frost"], strict=True)
     assert result.stdout.strip() == "Hello, Frost!"
+
+
+@pytest.mark.parametrize(
+    "req_str",
+    [
+        "-e file:///${PROJECT_ROOT}/plugins/test-plugin-pdm",
+        "-e test_plugin_pdm @ file:///${PROJECT_ROOT}/plugins/test-plugin-pdm",
+    ],
+)
+@pytest.mark.usefixtures("local_finder")
+def test_install_local_plugin_without_name(pdm, project, core, req_str):
+    import shutil
+
+    from . import FIXTURES
+
+    test_plugin_path = FIXTURES / "projects" / "test-plugin-pdm"
+    project.root.joinpath("plugins").mkdir(exist_ok=True)
+    shutil.copytree(test_plugin_path, project.root / "plugins" / "test-plugin-pdm", dirs_exist_ok=True)
+
+    project.pyproject.settings["plugins"] = [req_str]
+    project.pyproject.write()
+    with cd(project.root):
+        result = pdm(["install", "--plugins", "-vv"], obj=project, strict=True)
+
+        assert project.root.joinpath(".pdm-plugins").exists()
+        core.load_plugins()
+        result = pdm(["hello", "--name", "Frost"], strict=True)
+    assert result.stdout.strip() == "Hello, Frost"
