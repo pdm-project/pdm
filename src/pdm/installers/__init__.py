996629a406aada8623058b1d0e34c86ab7238bb9
Frost Ming <me@frostming.com>
2025-04-08 09:49:11 +0800

    fix: add __all__ attribute
    Signed-off-by: Frost Ming <me@frostming.com>

--- src/pdm/installers/__init__.py
+++ src/pdm/installers/__init__.py
@@ -1,5 +1,6 @@
-# flake8: noqa
+from pdm.installers.base import BaseSynchronizer
 from pdm.installers.manager import InstallManager
 from pdm.installers.synchronizers import Synchronizer
-from pdm.installers.base import BaseSynchronizer
 from pdm.installers.uv import UvSynchronizer
+
+__all__ = ["BaseSynchronizer", "InstallManager", "Synchronizer", "UvSynchronizer"]
