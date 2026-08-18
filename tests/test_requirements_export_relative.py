cc9aa1d0b8b12d46061b41c252b4115f12c226cb
Tung Lam <53996158+tunglambk@users.noreply.github.com>
2026-08-18 16:29:44 +0700

    test: simplify export options fixture

--- tests/test_requirements_export_relative.py
+++ tests/test_requirements_export_relative.py
@@ -1,3 +1,5 @@
+from argparse import Namespace
+
 from pdm.formats import requirements
 from pdm.models.requirements import parse_requirement
 from pdm.utils import cd
@@ -15,16 +17,7 @@ def test_export_editable_local_path_as_relative(project):
         req = parse_requirement("./packages/pkg-core", True)
     req.relocate(project.backend)
 
-    options = type(
-        "Options",
-        (),
-        {
-            "expandvars": False,
-            "hashes": False,
-            "self": False,
-            "editable_self": False,
-        },
-    )()
+    options = Namespace(expandvars=False, hashes=False, self=False, editable_self=False)
     result = requirements.export(project, [req], options)
 
     assert "-e ./packages/pkg-core#egg=pkg-core" in result.splitlines()
